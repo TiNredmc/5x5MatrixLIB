7781b6caebbb84aca5e9bde76d32e9b93104f717
TINLΞTHΛX <thipok17@hotmail.com>
2018-10-15 12:27:56 +0700

    Fixed some syntax

--- examples/TestYourDisp/TestYourDisp.ino
+++ examples/TestYourDisp/TestYourDisp.ino
@@ -1,14 +1,14 @@
 #include <5x5MatrixLIB.h> 
 
-5x5MatrixLIB Disp(2,3,4); //SDCLK DATA LOAD
+MatrixLIB Disp(2,3,4); //SDCLK DATA LOAD
 
 void setup()
 {
 Disp.begin();
 delay(500);
-Disp.setLamptest(on);
+Disp.setLampTest(true);
 delay(100);
-Disp.setLamptest(off);
+Disp.setLampTest(false);
 delay(100);
 Disp.setBrightness(100);
 Disp.clear();
@@ -16,7 +16,7 @@ Disp.clear();
 
 void loop()
 {
-Disp.writeStringScrolling("Testing...",);
+Disp.writeStringScrolling("Testing...",120);
 Disp.writeCharacter(0,'T');
 Disp.writeCharacter(1,'L');
 Disp.writeCharacter(2,'H');
