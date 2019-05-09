53da6b0d760254fe5f141ee39b6f121d7486949c
TINLΞTHΛX <thipok17@gmail.com>
2019-05-09 23:30:47 +0700

    Added bitmap drawing feature.
    Added bitmap drawing feature for the normal variable and PROGMEM's variable !

--- 5x5MatrixLIB.h
+++ 5x5MatrixLIB.h
@@ -18,6 +18,8 @@ class MatrixLIB {
 	void setBrightness(int percent);//set the brightness of the display 
 	void setPowerdownMode(boolean on);//Pwer saving mode of the display 
 	void setLampTest(boolean on);//Test the display lamp
+	void drawBitmap(int digit, const char data[]);// draw 5x5 bitmap image on an specified digit
+	void drawBitmapPgm(int digit, const char data[]);// draw 5x5 progmem's bitmap image on an specified digit
 	void bitbang(unsigned char code);//send the hex or binary to the display (SPI-like)
 };
 #endif
